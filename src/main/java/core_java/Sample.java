2e4552b59d15666474664988c48ac0d85b309caf
Manojkumar886 <maddymano434@gmail.com>
2024-03-08 12:42:16 +0530

    Basic,datatype is over..!

---
+++ src/main/java/core_java/Sample.java
@@ -0,0 +1,26 @@
+package core_java;
+
+public class Sample {
+    // main method
+    public static void main(String args[]) {
+        System.out.println("First java Programming Class");
+
+        String studentName = "Subash";
+        short studentAge = 26;
+        float studentHeight = 171.67f;
+        String studentNative = "Namakkal";
+        String studentMailid = "subash2145@gmail.com";
+        char studentBloodgroup = 'B';
+        long studentMobileno = 9789468347l;
+
+        System.out.println("---------------Student details-----------");
+
+        System.out.println("my name is :" + studentName);
+        System.out.println("my height is :" + studentHeight);
+        System.out.println("my blood group is :" + studentBloodgroup);
+
+        System.out.println(" my age is :" + studentAge + "\n my nativeplace is :" + studentNative + "\t my mail id is :"
+                + studentMailid);
+    }
+
+}
