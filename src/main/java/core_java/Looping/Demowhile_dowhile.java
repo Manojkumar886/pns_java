ab6d2e3cc32b76c7e5bba555c0f0408a51bdfb24
Manojkumar886 <maddymano434@gmail.com>
2024-03-22 12:29:56 +0530

    while_do while  completed....!

---
+++ src/main/java/core_java/Looping/Demowhile_dowhile.java
@@ -0,0 +1,27 @@
+package core_java.Looping;
+
+import java.util.Scanner;
+
+/**
+ * Demowhile_dowhile
+ */
+public class Demowhile_dowhile {
+    public static void main(String[] args) {
+        int i = 10;
+
+        while (i <= 1) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            i--;
+        }
+
+        int fav_number = 6;
+
+        do {
+            System.out.println(" this is even number");
+            fav_number -= 10;
+        } while (fav_number >= 10);
+
+    }
+}
